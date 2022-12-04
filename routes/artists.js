c18f94bc0f6407176b0cb1721c149c317f74b4b7
LucileTech <lucile.t.tech@gmail.com>
2022-12-04 16:18:36 +0100

    routes reorganized

---
+++ routes/artists.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const protectRoute = require("../middlewares/protectRoute");
+const uploader = require("../config/cloudinary");
+const Artist = require("../models/Artist.model");
+const Creation = require("../models/Creation.model");
+const Order = require("../models/Order.model");
+const { set } = require("mongoose");
+
+// Get all artists
+// The HTTP 200 OK success status response code indicates that the request has succeeded
+
+router.get("/artists", async (req, res, next) => {
+    const allArtists = await Artist.find();
+    res.status(200).json(allArtists);
+});
+
+
+// Create an artist
+// If there's already an artist page : (HTTP) 401 Unauthorized
+// If there's no artist page yet  : HTTP 201 Created success status response code indicates that the request has succeeded and has led to the creation of a resource
+
+router.post(
+    "/artists/form",
+    uploader.single("picture"), protectRoute, 
+    async (req, res, next) => {
+      const { name, description, user } = req.body;
+      let picture;
+      if (req.file) {
+        picture = req.file.path;
+      }
+  
+      const artistExists = await Artist.findOne({
+        user: req.currentUser.id,
+      })
+  
+      if (artistExists) {
+        console.log("One artist already exists")
+        return res.status(401).json({})
+      }
+  
+      const artist = await Artist.create({
+        name,
+        description,
+        picture,
+        user: req.currentUser.id,
+      });
+  
+      res.status(201).json(artist);
+    }
+  );
+
+
+module.exports = router;
---
+++ routes/orders.js
@@ -0,0 +1,221 @@
+const router = require("express").Router();
+const protectRoute = require("../middlewares/protectRoute");
+const uploader = require("../config/cloudinary");
+const Artist = require("../models/Artist.model");
+const Creation = require("../models/Creation.model");
+const Order = require("../models/Order.model");
+const { set } = require("mongoose");
+
+// Get all orders of current user
+// The HTTP 200 OK success status response code indicates that the request has succeeded
+
+router.get("/orders", protectRoute, async (req, res, next) => {
+    const allCreations = await Order.find({
+      userId: req.currentUser.id,
+      date: { $exists: true },
+    });
+    res.status(200).json(allCreations);
+});
+ 
+// Get an orderCart if order without date exists
+// The HTTP 200 OK success status response code indicates that the request has succeeded
+
+router.get("/orderCart", protectRoute, async (req, res, next) => {
+    const orderCart = await Order.findOne({
+      userId: req.currentUser.id,
+      date: { $exists: false },
+    });
+    console.log(orderCart);
+    res.status(200).json(orderCart);
+});
+  
+
+// Create an order if !order
+// HTTP 201 Created success status response code indicates that the request has succeeded and has led to the creation of a resource
+// router.post("/order", async (req, res) => {
+//     const { userId, creations, amount, date } = req.body;
+  
+//     const newOrder = await Order.create({
+//       userId,
+//       creations,
+//       amount,
+//       date,
+//     });
+//     res.status(201).json(newOrder);
+// });
+
+
+// Add one creation to OrderCart
+// HTTP 201 Created success status response code indicates that the request has succeeded and has led to the creation of a resource
+
+router.post(
+    "/creations/:id/addtocart",
+    protectRoute,
+    async (req, res, next) => {
+      try {
+        console.log(req.params.id);
+        const order = await Order.findOne({
+          userId: req.currentUser.id,
+          date: { $exists: false },
+        });
+        if (order) {
+          let updated = false;
+          order.creations.forEach((creation) => {
+            if (creation.productId.toString() === req.params.id) {
+              creation.quantity++;
+              updated = true;
+            }
+          });
+          if (!updated) {
+            order.creations.push({
+              productId: req.params.id,
+              quantity: 1,
+            });
+          }
+          await order.save();
+        } else {
+          const newOrder = await Order.create({
+            userId: req.currentUser.id,
+            creations: [
+              {
+                productId: req.params.id,
+                quantity: 1,
+              },
+            ],
+          });
+          return res.status(201).json(newOrder);
+        }
+  
+        res.status(201).json(order);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+);
+
+// Delete one creation in order
+// HTTP 202 Accepted 
+
+router.patch("/orderCart/:id", protectRoute, async (req, res, next) => {
+    const { id } = req.params;
+    console.log(id);
+    try {
+      const updatedOrder = await Order.findOneAndUpdate(
+        {
+          userId: req.currentUser.id,
+          date: { $exists: false },
+        },
+        {
+          $pull: { creations: { productId: id } },
+        },
+        {
+          new: true,
+        }
+      );
+  
+      res.status(202).json(updatedOrder);
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  // Delete the entire cart order 
+  // HTTP 204 No Content success status response code indicates that a request has succeeded, but that the client doesn't need to navigate away from its current page
+  
+  router.delete("/orderCart/delete", protectRoute, async (req, res, next) => {
+    try {
+      let orderDeleted = await Order.findOneAndDelete({
+        userId: req.currentUser.id,
+        date: { $exists: false },
+      });
+      res.status(204).json(orderDeleted);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+
+// Add an amount of creation in cart
+// HTTP 201 Created success status response code indicates that the request has succeeded and has led to the creation of a resource
+router.patch(
+    "/orderCart/increment/:creationId",
+    protectRoute,
+    async (req, res, next) => {
+      try {
+        console.log(req.params.creationId);
+        const order = await Order.findOne({
+          userId: req.currentUser.id,
+          date: { $exists: false },
+        });
+        if (order) {
+          let updated = false;
+          order.creations.forEach((creation) => {
+            if (creation.productId.toString() === req.params.creationId) {
+              creation.quantity++;
+              updated = true;
+            }
+          });
+          await order.save();
+        }
+        res.status(201).json(order);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+
+// Remove an amount of creation in cart
+// HTTP 201 Created success status response code indicates that the request has succeeded and has led to the creation of a resource
+
+router.patch(
+    "/orderCart/decrement/:creationId",
+    protectRoute,
+    async (req, res, next) => {
+      try {
+        console.log(req.params.creationId);
+        const order = await Order.findOne({
+          userId: req.currentUser.id,
+          date: { $exists: false },
+        });
+        if (order) {
+          let updated = false;
+          order.creations.forEach((creation) => {
+            if (creation.productId.toString() === req.params.creationId) {
+              if (creation.quantity > 0) {
+                creation.quantity--;
+                updated = true;
+              }
+              if (creation.quantity === 0) {
+                order.creations.splice(order.creations.indexOf(creation), 1);
+              }
+            }
+          });
+          await order.save();
+        }
+        res.status(201).json(order);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  );
+
+
+
+// Buy an Ordercart // add date
+// The HyperText Transfer Protocol (HTTP) 202 Accepted response status code indicates that the request has been accepted for processing
+
+router.put("/orderCart/buy", protectRoute, async (req, res, next) => {
+    try {
+      const findOrder = await Order.findOne({
+        userId: req.currentUser.id,
+        date: { $exists: false },
+      });
+      findOrder.date = new Date();
+      await findOrder.save();
+      res.status(202).json(findOrder);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+module.exports = router;
