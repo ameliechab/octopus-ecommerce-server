f24eeb21f63253815e221afb3814d2b58b399923
Amélie Chabrand <amelie.chabrand@gmail.com>
2022-12-05 12:47:24 +0100

    get for one creation and one artist

--- routes/artists.js
+++ routes/artists.js
@@ -10,8 +10,16 @@ const { set } = require("mongoose");
 // The HTTP 200 OK success status response code indicates that the request has succeeded
 
 router.get("/artists", async (req, res, next) => {
-    const allArtists = await Artist.find();
-    res.status(200).json(allArtists);
+  const allArtists = await Artist.find();
+  res.status(200).json(allArtists);
+});
+
+// Get one artist
+// The HTTP 200 OK success status response code indicates that the request has succeeded
+
+router.get("/artists/:id", async (req, res, next) => {
+  const oneArtist = await Artist.findById(req.params.id);
+  res.status(200).json(oneArtist);
 });
 
 // Get my artist
@@ -29,36 +37,34 @@ router.get("/myartist", protectRoute, async (req, res, next) => {
 // If there's no artist page yet  : HTTP 201 Created success status response code indicates that the request has succeeded and has led to the creation of a resource
 
 router.post(
-    "/artists/form",
-    uploader.single("picture"), protectRoute, 
-    async (req, res, next) => {
-      const { name, description, user } = req.body;
-      let picture;
-      if (req.file) {
-        picture = req.file.path;
-      }
-  
-      const artistExists = await Artist.findOne({
-        user: req.currentUser.id,
-      })
-  
-      if (artistExists) {
-        console.log("One artist already exists")
-        return res.status(401).json({})
-      }
-  
-      const artist = await Artist.create({
-        name,
-        description,
-        picture,
-        user: req.currentUser.id,
-      });
-  
-      res.status(201).json(artist);
+  "/artists/form",
+  uploader.single("picture"),
+  protectRoute,
+  async (req, res, next) => {
+    const { name, description, user } = req.body;
+    let picture;
+    if (req.file) {
+      picture = req.file.path;
     }
-  );
 
+    const artistExists = await Artist.findOne({
+      user: req.currentUser.id,
+    });
 
+    if (artistExists) {
+      console.log("One artist already exists");
+      return res.status(401).json({});
+    }
 
+    const artist = await Artist.create({
+      name,
+      description,
+      picture,
+      user: req.currentUser.id,
+    });
+
+    res.status(201).json(artist);
+  }
+);
 
 module.exports = router;
